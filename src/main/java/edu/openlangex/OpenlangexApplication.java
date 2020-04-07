ccb1a4e8ab751594375a0ba600cf3ec5a99290c6
David Veiga <davidaug23.7@gmail.com>
2020-04-07 16:08:32 -0300

    First Commit

---
+++ src/main/java/edu/openlangex/OpenlangexApplication.java
@@ -0,0 +1,13 @@
+package edu.openlangex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlangexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenlangexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/openlangex/OpenlangexApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.openlangex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenlangexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
